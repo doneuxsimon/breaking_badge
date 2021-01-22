80d043cb8a4ed46aaaabfb6f9fac8d37c7bb591a
Alexandre Albelice <albelice.alex@gmail.com>
2021-01-22 15:41:24 +0100

    css update stud

--- template/pages/students.php
+++ template/pages/students.php
@@ -1,10 +1,7 @@
 <?php
-
 require_once('./components/functions.php');
 $users = getStudents();
 while ($user = $users->fetch()) {
-    echo "<pre>";
-    print_r($user);
-    echo "</pre>";
+    echo $user['firstname'] . " " . $user['lastname'] . " : " . $user['mail'] . "<br>";
 }
 ?>
